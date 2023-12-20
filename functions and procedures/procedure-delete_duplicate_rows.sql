1ec7ba5e551fc200eadafdfbd12e2943fbca2edd
saya-loo <90766515+saya-loo@users.noreply.github.com>
2023-12-20 17:26:46 +0600

    ..

--- functions and procedures/procedure-delete_duplicate_rows.sql
+++ functions and procedures/procedure-delete_duplicate_rows.sql
@@ -1,20 +1,28 @@
-CREATE OR REPLACE PROCEDURE delete_duplicate_rows (
-	p_table_name IN VARCHAR2,
-	p_column_name IN VARCHAR2
+CREATE OR REPLACE PROCEDURE delete_duplicate_payments (
+    p_table_name IN VARCHAR2,
+    p_payment_id_column IN VARCHAR2
 ) AS
+    v_sql VARCHAR2(1000);
 BEGIN
-	FOR dup_row IN (SELECT MIN(rowid) AS min_rowid
-                        FROM p_table_name
-                        GROUP BY p_column_name
-                        HAVING COUNT(*) > 1)
-        LOOP 
-            DELETE FROM p_table_name
-	    WHERE ROWID = dup_row.min_rowid;
-        END LOOP;
-END delete_duplicate_rows;
+    v_sql := 'DELETE FROM ' || p_table_name ||
+             ' WHERE ' || p_payment_id_column || ' IN ' ||
+             '(SELECT ' || p_payment_id_column || ' FROM ' ||
+             '(SELECT ' || p_payment_id_column || ', ROW_NUMBER() OVER (PARTITION BY ' ||
+             p_payment_id_column || ' ORDER BY rowid) AS rnum FROM ' || p_table_name ||
+             ') WHERE rnum > 1)';
+
+    EXECUTE IMMEDIATE v_sql;
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('Duplicate payments deleted successfully.');
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+        ROLLBACK;
+END delete_duplicate_payments;
 /
 
 BEGIN
-	delete_duplicate_rows('PAYMENT', 'PAYMENT_ID');
+    delete_duplicate_payments('PAYMENT', 'PAYMENT_ID');
 END;
 /
