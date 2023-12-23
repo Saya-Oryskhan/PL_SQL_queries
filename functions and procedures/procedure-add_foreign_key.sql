20079ac93506bd3babeae6d5d0960d634351abbc
saya-loo <90766515+saya-loo@users.noreply.github.com>
2023-12-23 13:09:27 +0600

    ..

--- functions and procedures/procedure-add_foreign_key.sql
+++ functions and procedures/procedure-add_foreign_key.sql
@@ -8,3 +8,43 @@ BEGIN
   add_foreign_key;
 END;
 /
+
+
+
+
+CREATE OR REPLACE PROCEDURE remove_constraint AS
+BEGIN
+  EXECUTE IMMEDIATE 'ALTER TABLE PRESCRIPTION_detail DROP CONSTRAINT fk_prescription';
+  EXECUTE IMMEDIATE 'ALTER TABLE PRESCRIPTION DROP CONSTRAINT pk_prescription';
+  DBMS_OUTPUT.PUT_LINE('Foreign key and primary key constraints dropped successfully.');
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+END remove_constraint;
+/
+
+BEGIN
+  remove_constraint;
+END;
+/
+
+
+
+
+
+
+CREATE OR REPLACE PROCEDURE remove_primary_key(p_table_name VARCHAR2) AS
+BEGIN
+  EXECUTE IMMEDIATE 'ALTER TABLE ' || p_table_name || ' DROP PRIMARY KEY';
+  
+  DBMS_OUTPUT.PUT_LINE('Primary key dropped successfully.');
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+END remove_primary_key;
+/
+
+BEGIN
+  remove_primary_key('your_table_name');
+END;
+/
